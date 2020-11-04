7e631157c53b0ee5f7626ef82f26218194a62cf3
vmo-campos <viniciusmoliveirac@hotmail.com>
2020-11-03 21:46:53 -0300

    Passing in tests

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -11,7 +11,23 @@ interface Balance {
 @EntityRepository(Transaction)
 class TransactionsRepository extends Repository<Transaction> {
   public async getBalance(): Promise<Balance> {
-    // TODO
+    const transactions = await this.find();
+
+    const { income, outcome } = transactions.reduce(
+      (a, t) => {
+        if(t.type === 'income')
+          a.income += Number(t.value);
+
+        if(t.type === 'outcome')
+          a.outcome += Number(t.value);
+        return a;
+      },
+      { income: 0, outcome: 0, total: 0 },
+    );
+
+    const total = income - outcome;
+
+    return { income, outcome, total };
   }
 }
 
